789d13088befa3c7bb3ab3a8aee44b8e76516362
Arpita <arpita@Arpitas-MBP.wv.cc.cmu.edu>
2017-03-03 20:17:28 -0500

    refactoring word lstm and some generated text

--- restricted-dataset/scripts/k1word.py
+++ restricted-dataset/scripts/k1word.py
@@ -52,6 +52,7 @@ for line in train:
 print('nb sequences:', len(sentences))
 print (len(sentences))
 
+
 print('Vectorization...')
 print(len(sentences))
 print(maxlen)
@@ -84,70 +85,3 @@ with open("model_word_lstm.json", "w") as json_file:
 model.save_weights("model_word_lstm.h5")
 print("Saved model to disk")
 
-
-perplexity = 0
-N = 0
-
-for i in range(len(test)):
-    w = test[i].split()
-    for j in range(len(w)-maxlen):
-        bigram = w[j:j+maxlen]
-        x = np.zeros((1, maxlen, len(words)))
-        for t, word in enumerate(bigram):
-            x[0, t, word_indices[word]] = 1.
-
-        preds = model.predict(x, verbose=0)[0]
-        next_word = w[j+maxlen]
-        prob = preds[word_indices[next_word]]
-        #import pdb;pdb.set_trace()
-        #print (prob,i)
-        perplexity += np.log(prob)
-        N += 1
-
-perplexity /= -N
-print (perplexity)
-
-exit(1)
-def sample(preds, temperature=1.0):
-    # helper function to sample an index from a probability array
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
-
-# train the model, output generated text after each iteration
-for iteration in range(1, 4):
-    print()
-    print('-' * 50)
-    print('Iteration', iteration)
-    model.fit(X, y, batch_size=7, nb_epoch=1)
-
-    start_index = random.randint(0, len(text) - maxlen - 1)
-
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print()
-        print('----- diversity:', diversity)
-
-        generated = ''
-        sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(generated)
-
-        for i in range(400):
-            x = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x[0, t, char_indices[char]] = 1.
-
-            preds = model.predict(x, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
-
-            generated += next_char
-            sentence = sentence[1:] + next_char
-
-            sys.stdout.write(next_char)
-            sys.stdout.flush()
-        print()
